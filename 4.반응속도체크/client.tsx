945e36691d236b7a0a3ec40badd8b237b9386a7f
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-04-05 01:22:34 +0900

    Modify : 폴더삭제

--- 4.반응속도체크/client.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import ResponseCheck from './ResponseCheck';
-
-ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
