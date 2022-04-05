ae6bfb6275ee504e3701a5a6e80323335c861f34
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-04-05 12:58:28 +0900

    Add : lotto

--- lecture/client.tsx
+++ lecture/client.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader/root";
 
-import RSP from "./RSP";
+import Lotto from "./Lotto";
 
-const Hot = hot(RSP);
+const Hot = hot(Lotto);
 
 ReactDOM.render(<Hot />, document.querySelector("#root"));
