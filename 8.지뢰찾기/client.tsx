a7b6589a3416d96ebec6348955bb4fd06c31ac3f
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-04-07 03:33:26 +0900

    Modify : 중복파일 삭제

--- 8.지뢰찾기/client.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import MineSearch from './MineSearch';
-
-ReactDOM.render(<MineSearch />, document.querySelector('#root'));
